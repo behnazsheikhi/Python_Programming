8085037d6dfd744273ec557958632be1edbcb43e
b.sheikhi <behnaz.sheikhi@gmail.com>
2020-08-01 10:38:42 +0430

    tupleSyntax

---
+++ tuple.py
@@ -0,0 +1,7 @@
+programming_language=("python","Java","C++","C#")
+print(type(programming_language))
+
+programming_language="python","Java","C++","C#"
+for languages in programming_language:
+    print(languages)
+
